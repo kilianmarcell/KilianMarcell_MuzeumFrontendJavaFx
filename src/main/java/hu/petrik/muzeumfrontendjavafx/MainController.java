3d60211ec5bf6603004bc1da588ff3d25b2215f5
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-17 13:35:04 +0100

    Fájlok átnevezve

--- src/main/java/hu/petrik/muzeumfrontendjavafx/MainController.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/MainController.java
@@ -3,7 +3,7 @@ package hu.petrik.muzeumfrontendjavafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
